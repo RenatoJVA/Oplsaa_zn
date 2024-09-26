ba30a2b1a10164e2a1eea415c57305042a49627a
Renato <renato.vace@gmail.com>
2024-09-26 14:26:31 -0500

    ju., 26 de sep. de 2024 14:26:31

--- Metalloprotein/00_Pre.sh
+++ Metalloprotein/00_Pre.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+#Variables
+protein = "PARP.pdb"
+prot=$(basename $protein.pdb)
+forcefield = "oplsaa_zn.ff"
+solvate = true
+ions = true
+
+backupProt(){
+  cp protein/${prot}.pdb $prot.pdb
+  cp ${prot}.pdb ${prot}_B.pdb
+}
+
+
+
+
